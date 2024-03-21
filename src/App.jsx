bc2685e4b855fd0536ef3720264535cd0c44f37f
Oğuzhan Yıldırım <yildirimogcod@gmail.com>
2024-03-21 19:05:14 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
